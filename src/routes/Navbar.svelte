61a1d2294d413ade7e4bc5036f9bc1af3810b5b7
Luca Di Marco <lucanahtsu@gmail.com>
2023-10-21 23:56:27 -0300

    Add navbar

---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,4 @@
+<nav class="bg-gradient-to-b from-richblack to-richblack/60 h-[10vh] flex items-center justify-between px-5">
+    <a href="./" class="text-2xl"><span class="font-bold">LAGGY</span> or not</a>
+    <a href="https://github.com/tsuramii" target="_blank">Made by tsu</a>
+</nav>
