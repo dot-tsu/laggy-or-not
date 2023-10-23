87b4e0655c79c3747bbff16ed1bb0e2eb25ac6dc
Luca Di Marco <lucanahtsu@gmail.com>
2023-10-23 18:46:32 -0300

    Add static html for game selector

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,28 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+<main class="bg-richblack/60 min-h-[90vh]">
+    <h1 class="font-black text-center">
+        <span class="text-8xl">LAGGY</span><span
+            class="font-normal text-4xl block">OR NOT</span
+        >
+    </h1>
+    <section class="m-10 text-xl p-5">
+        <h2 class="py-5 text-4">Select a game</h2>
+        <div class="grid gap-5 [&>article]:relative [&>article]:cursor-pointer [&>article]:shadow-xl [&>article>img]:rounded-lg [&>article>img]:h-full [&>article>img]:max-w-full [&>article]:rounded-lg [&>article>h3]:rounded-b-lg [&>article>h3]:absolute [&>article>h3]:bottom-0 [&>article>h3]:p-5 [&>article>h3]:bg-richblack/50 [&>article>h3]:w-full">
+            <article>
+                <img src="https://videogames.si.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTk1MDM4MTM2MzAzMTY3MjAy/league-of-legends-key-art.jpg" alt="League of Legends" />
+                <h3>League of Legends</h3>
+            </article>
+            <article>
+                <img src="https://firstsportz.com/wp-content/uploads/2022/12/Valorant-year-end-1.jpg" alt="Valorant" />
+                <h3>Valorant</h3>
+            </article>
+            <article>
+                <img src="https://readcbr.com/wp-content/uploads/2023/09/counter-strike-2.jpg" alt="Counter-Strike 2" />
+                <h3>Counter-Strike 2</h3>
+            </article>
+            <article>
+                <img src="https://www.playerone.vg/wp-content/uploads/2021/08/overwatch2-1-e1628508798686.jpg" alt="Overwatch 2" />
+                <h3>Overwatch 2</h3>
+            </article>
+        </div>
+    </section>
+</main>
