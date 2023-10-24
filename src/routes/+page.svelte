f2981582575b714b917571cd26ef15bb79438e3b
Luca Di Marco <lucanahtsu@gmail.com>
2023-10-23 21:53:59 -0300

    Add server selection and ping button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,12 @@
       image:
         "https://videogames.si.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTk1MDM4MTM2MzAzMTY3MjAy/league-of-legends-key-art.jpg",
       available: true,
+      ips: {
+        LAS: "151.106.249.1",
+        LAN: "104.160.136.3",
+        EUW: "104.160.141.3",
+        NA: "104.160.131.3",
+      },
     },
     {
       name: "Valorant",
@@ -25,6 +31,8 @@
       available: false,
     },
   ];
+
+  let selectedGame = games[0];
 </script>
 
 <main class="bg-richblack/60 min-h-[90vh]">
@@ -33,7 +41,26 @@
       >OR NOT</span
     >
   </h1>
+  <section class="m-10 text-xl p-5">
+    <label for="server-select">Select your server</label>
 
+    <form id="ping-form">
+      <select
+        id="server-select"
+        name="server"
+        class="block my-5 rounded-sm border-2 border-white p-2 bg-richblack/50 w-full"
+      >
+        <option value="" disabled selected>Please choose an option</option>
+        {#each Object.keys(selectedGame.ips) as ip}
+          <option value={selectedGame.ips[ip]}>{ip}</option>
+        {/each}
+      </select>
+      <button
+        class="w-full bg-richblack/50 rounded-sm p-2 border-2 border-white hover:bg-white hover:text-richblack transition-colors"
+        >Ping!</button
+      >
+    </form>
+  </section>
   <section class="m-10 text-xl p-5">
     <h2 class="py-5 text-4">Select a game</h2>
     <div
@@ -42,8 +69,11 @@
       {#each games as { name, image, available }}
         <article
           class="{available
-            ? "cursor-pointer hover:scale-105 transition-transform"
-            : "[&>img]:grayscale cursor-not-allowed"}"
+            ? 'cursor-pointer hover:scale-105 transition-transform'
+            : '[&>img]:grayscale cursor-not-allowed'} {selectedGame.name ===
+          name
+            ? 'border-2 border-x-flame '
+            : null}"
         >
           <img src={image} alt={name} />
           <h3>{name} {available ? "" : "(Work in progress)"}</h3>
