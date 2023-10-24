9356ba9266a86aa30029844d7c93e753f8baabfb
Luca Di Marco <lucanahtsu@gmail.com>
2023-10-23 22:06:08 -0300

    Add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
     import "../app.css";
     import Nav from "./Navbar.svelte";
+    import Footer from "./Footer.svelte";
 </script>
   <Nav />
 <slot />
+<Footer/>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,3 @@
+<footer class="text-center p-5 bg-gradient-to-t from-richblack to-richblack/60">
+    <p>Made with ♥ by <a href="https://github.com/tsuramii" class="hover:text-atomictangerine transition-colors underline underline-offset-2" target="_blank">tsu</a></p>
+</footer>
