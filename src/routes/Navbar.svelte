1d0059dce2f01714d4d5ccc755cd352acbb1e1a7
Luca Di Marco <lucanahtsu@gmail.com>
2023-10-26 19:03:36 -0300

    Improve logo

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,4 +1,4 @@
 <nav class="bg-gradient-to-b from-richblack to-richblack/60 h-[10vh] flex items-center justify-between px-5">
-    <a href="./" class="text-2xl"><span class="font-bold">LAGGY</span> or not</a>
+    <a href="./" class="text-2xl font-bold">LAGGY <span class="font-light">or</span> NOT</a>
     <a href="https://github.com/tsuramii" target="_blank">Made by tsu</a>
 </nav>
